b4de6b83c8444a4b0104b96a7735750e0a4035e2
Yimin Arava <85930454+YiminArava4508@users.noreply.github.com>
2024-01-06 21:47:05 -0700

    Fix window size for smaller screen
    Change window scaling to match different window sizes

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,28 +7,30 @@ const About = () => {
     return (
         <>
         <div className='container about-page'>
-            <h1>
-                About me
-            </h1>
-            <p>I graduated from the 
-                University of California-Santa Barbara in june 2023
-                with a B.S. degree in Statistics and Data Science.
-                <br></br>
-                <br></br>
-                I am currently seeking entry-level Data Science/ Data Engineering Positions 
-                where I can apply my Data Science and Software skills 
-                to solve real-world problems and with innovative solutions. 
-                <br></br>
-                <br></br>
-                I am eager to connect with fellow 
-                Data Scientists, Engineers, Analysts, and Professionals 
-                who also share my desire to learn, improve, and make impactful changes 
-                in this data-driven world. 
-                <br></br>
-                <br></br>
-                Please feel free to reach out if you want to discuss any data-related topics or opportunities.
-                </p>
-
+            <div className="text-zone" >
+                <h1>
+                    About me
+                </h1>
+                    <p>
+                    I graduated from the 
+                    University of California-Santa Barbara in june 2023
+                    with a B.S. degree in Statistics and Data Science.
+                    <br></br>
+                    <br></br>
+                    I am currently seeking entry-level Data Science/ Data Engineering Positions 
+                    where I can apply my Data Science and Software skills 
+                    to solve real-world problems and with innovative solutions. 
+                    <br></br>
+                    <br></br>
+                    I am eager to connect with fellow 
+                    Data Scientists, Engineers, Analysts, and Professionals 
+                    who also share my desire to learn, improve, and make impactful changes 
+                    in this data-driven world. 
+                    <br></br>
+                    <br></br>
+                    Please feel free to reach out if you want to discuss any data-related topics or opportunities.
+                    </p>
+            </div>
         </div>
         <Loader type ="pacman" />
         </>
